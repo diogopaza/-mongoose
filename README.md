bf76561d385ebadeeb78baf219ba3cb5f387dcf3
Diogo Paza <diogopazacvel@gmail.com>
2018-01-14 15:46:05 -0200

    cont

---
+++ README.md
@@ -0,0 +1,4 @@
+
+https://bognarjunior.wordpress.com/2016/01/30/nodejs-conectando-ao-mongodb-com-mongoose/
+
+mongodb puro e simples sem frameworks, gravando tabelas simples
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blog');
+
+var Faculdade = mongoose.model('Faculdades',{nome:String,sigla:String });
+var novaFaculdade = new Faculdade({nome:'Fag',sigla:'FAG'});
+var faculdade1 = new Faculdade({nome: 'união cascavel', sigla :'UUUUU'});
+var astolfo = new Faculdade({nome: 'Astolfo', sigla :'PPPPP'});
+
+novaFaculdade.save(function(err){
+    if(err) throw err;
+    console.log('Dados salvos com sucesso');
+})
+
+faculdade1.save(function(err){
+    if(err) throw err;
+    console.log('Dados salvos com sucesso');
+})
+
+astolfo.save(function(err){
+    if(err) throw err;
+    console.log('Dados salvos com sucesso');
+})
